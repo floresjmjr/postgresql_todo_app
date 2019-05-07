01588e93733cb73a517edfcffee35e03728fa9d2
Jorge <jorge@Jorges-MacBook-Pro.local>
2019-05-07 00:45:59 -0400

    database config switch

--- config/database.js
+++ config/database.js
@@ -1,30 +1,30 @@
 const Sequelize = require('sequelize')
 
 // Option 1 (localhost)
-module.exports = new Sequelize('todos', null, null, {
-  host: 'localhost',
-  dialect: 'postgres',
+// module.exports = new Sequelize('todos', null, null, {
+//   host: 'localhost',
+//   dialect: 'postgres',
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
 
-  define: {
-    timestamps: false,
-  },
-
-})
-
-
-// Option 2 (Heroku)
-// module.exports = new Sequelize(process.env.DATABASE_URL, {
-  
 //   define: {
 //     timestamps: false,
 //   },
 
-// });
+// })
+
+
+// Option 2 (Heroku)
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  
+  define: {
+    timestamps: false,
+  },
+
+});
 
